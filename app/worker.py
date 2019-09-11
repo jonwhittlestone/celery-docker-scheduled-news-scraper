7fcd22da1a8375404a5b2db800e478dfe1c7353f
Jon Whittlestone <dev@howapped.com>
2019-09-11 20:44:52 +0100

    initial commit - scheduled article scraper

---
+++ app/worker.py
@@ -0,0 +1,13 @@
+# worker.py
+import os
+from celery import Celery
+
+app = Celery(include=('tasks',))
+
+app.conf.beat_schedule = {
+  'refresh': {
+    'task': 'refresh',  
+    'schedule': float(os.environ['NEWSPAPER_SCHEDULE']),
+    'args': (os.environ['NEWSPAPER_URLS'].split(','),)
+  }
+}
